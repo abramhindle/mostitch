db5f1d24914dd80e11c6bad6ed9ce44e6358ec1d
Abram Hindle <abram.hindle@softwareprocess.es>
2013-04-03 23:24:14 -0600

    in the middle

--- zmqtkclient.py
+++ zmqtkclient.py
@@ -14,35 +14,57 @@ def send_change(state):
         return res
 
 
+
+
 class App:     
     def _update(self):
         self.state = send_change(self.state)
     def minchange(self,a1):
-	self.state["mingrains"] = int(a1)
-	self.__correct_grains()
+        self.state["mingrains"] = int(a1)
+        self.__correct_grains()
         self._update()
         return 1
     def __correct_grains(self):
-	mingrains = min(self.state["mingrains"], self.state["maxgrains"])
-	maxgrains = max(self.state["mingrains"], self.state["maxgrains"])
-	self.state["mingrains"] = mingrains
-	self.state["maxgrains"] = maxgrains
+        mingrains = min(self.state["mingrains"], self.state["maxgrains"])
+        maxgrains = max(self.state["mingrains"], self.state["maxgrains"])
+        self.state["mingrains"] = mingrains
+        self.state["maxgrains"] = maxgrains
     def maxchange(self,a1):
-	self.state["maxgrains"] = int(a1)
-	self.__correct_grains()
+        self.state["maxgrains"] = int(a1)
+        self.__correct_grains()
         self._update()
         return 1
     def ampchange(self,a1):
-	self.state["amp"] = int(a1)/1000.0
+        self.state["amp"] = int(a1)/1000.0
         self._update()
     def topnchange(self,a1):
-	self.state["topn"] = int(a1)
+        self.state["topn"] = int(a1)
         self._update()
     def delaychange(self,a1):
-	self.state["delay"] = int(a1)
+        self.state["delay"] = int(a1)
         self._update()
+    def dflcommand(self, statename, a1):
+        self.state[statename] = int(a1)
+        self._update()
+    def make_slider(self,name,statename,from_=0,to=1000,command=None):
+        labelname = statename + "_label"
+        self.widgets[labelname] = Label(frame,text=name)
+        self.widgets[labelname].grid(row=curr_row,column=0,sticky=self.maxsticky)
+        dflcommand = lambda(x): self.dflcommand(statename,x)
+        command = dflcommand if command == None
+        self.widgets[statename] = Scale(
+                frame, 
+                from_=from_,
+                to=to,
+                command=dflcommand,
+                orient=HORIZONTAL)
+        self.widgets[statename].grid(row=curr_row,column=1,sticky=self.maxsticky)
+        self.curr_row += 1
+  
     def __init__(self, master):
-	maxsticky = N+S+E+W
+        self.curr_row = 0
+        self.widgets = {}
+        self.maxsticky = N+S+E+W
         frame = Frame(master)
         frame.grid(sticky=maxsticky)
         top=master.winfo_toplevel()
@@ -56,38 +78,35 @@ class App:
         frame.rowconfigure(2, weight=1)
         frame.columnconfigure(0, weight=1)
         frame.columnconfigure(1, weight=1)
-	self.state = {
-		"maxgrains":100,
-		"mingrains":10,
-		"amp":20,
+        self.state = {
+                "maxgrains":100,
+                "mingrains":10,
+                "amp":20,
                 "delay":1024*3
-	}
-	
-        self.minlabel = Label(frame,text="Min Grain")
-	self.minlabel.grid(row=0,column=0,sticky=maxsticky)
-        self.min = Scale(frame, to=1000,command=self.minchange,orient=HORIZONTAL)
-        self.min.grid(row=0,column=1,sticky=maxsticky)
-	
+        }
+        self.make_slider("Min Grain", "mingrains", command=self.minchange)
+        self.make_slider("Max Grain", "maxgrains", command=self.maxchange)
+        
         self.maxlabel = Label(frame,text="Max Grain")
-	self.maxlabel.grid(row=1,column=0,sticky=maxsticky)
+        self.maxlabel.grid(row=1,column=0,sticky=maxsticky)
         self.max = Scale(frame, to=1000,command=self.maxchange,orient=HORIZONTAL)
         self.max.grid(row=1,column=1,sticky=maxsticky)
-	
+        
         self.label = Label(frame,text="Amp")
-	self.label.grid(row=2,column=0,sticky=maxsticky)
+        self.label.grid(row=2,column=0,sticky=maxsticky)
         self.amp = Scale(frame, to=1000,command=self.ampchange,orient=HORIZONTAL)
         self.amp.grid(row=2,column=1,sticky=maxsticky)
         self.label = Label(frame,text="Top N")
-	self.label.grid(row=3,column=0,sticky=maxsticky)
+        self.label.grid(row=3,column=0,sticky=maxsticky)
         self.topn = Scale(frame, from_=1, to=25,command=self.topnchange,orient=HORIZONTAL)
         self.topn.grid(row=3,column=1,sticky=maxsticky)
         self.delaylabel = Label(frame,text="Max Delay")
-	self.delaylabel.grid(row=4,column=0,sticky=maxsticky)
+        self.delaylabel.grid(row=4,column=0,sticky=maxsticky)
         self.delay = Scale(frame, from_=1, to=44100,command=self.delaychange,orient=HORIZONTAL)
         self.delay.grid(row=4,column=1,sticky=maxsticky)
         
         
-	self.frame = frame
+        self.frame = frame
 
 
 
