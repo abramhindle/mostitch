b24a412a3df6eb4141c03e75853365019c86d9ca
Abram Hindle <abram.hindle@softwareprocess.es>
2014-01-17 16:50:36 -0700

    playfm

--- fm/playfm.sh
+++ fm/playfm.sh
@@ -1,2 +1,2 @@
 cd ..
-python print_stitch.py  fm/fmout.wav | perl fm/fmmapper.pl fm/map.dump  | csound -L stdin -o devaudio -+rtaudio=jack -dm6 ./fm/fm.orc ./fm/fmlive.sco
+python print_stitch.py  fm/fmout.wav | perl fm/fmmapper.pl fm/map.dump  | csound -L stdin -o devaudio -+rtaudio=jack -B 2048 -dm6 ./fm/fm.orc ./fm/fmlive.sco
